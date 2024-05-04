6c394dbebfa38bea3dcaa1f5b4d6973560b7c85a
madhavsGIT <kmadhav455@gamil.com>
2024-05-04 19:13:30 +0530

    commit

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -4,28 +4,35 @@ public class Group {
     private String name;
     private int numberOfParticipants;
 
-    public Group(String name, int numberOfParticipants ) {
-
-        this.numberOfParticipants = numberOfParticipants;
-        this.name = name;
-    }
 
     public Group() {
+    }
+
+    public Group(String name, int numberOfParticipants ) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+
+    }
+
+
+
+
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
     }
 
     public int getNumberOfParticipants() {
         return numberOfParticipants;
     }
 
-    public void setNumberOfParticipants(int numberOfParticipants) {
-        this.numberOfParticipants = numberOfParticipants;
-    }
+
 }
